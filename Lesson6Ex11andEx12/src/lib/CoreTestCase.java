09846f934cec9105180511bf86695a478d69aec6
Nikolay Gorbachev <nagorbachev@kp11.ru>
2020-02-02 11:36:27 +0300

    Ex11Ex12oldapp
    Ex11Ex12oldapp

---
+++ Lesson6Ex11andEx12/src/lib/CoreTestCase.java
@@ -0,0 +1,56 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import junit.framework.TestCase;
+import lib.ui.WelcomePageObject;
+import org.openqa.selenium.ScreenOrientation;
+import java.time.Duration;
+
+public class CoreTestCase extends TestCase {
+
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
+
+    protected AppiumDriver driver;
+
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        driver = Platform.getInstance().getDriver();
+        this.rotateScreenPortrait();
+        this.skipWelcomePageForIOSApp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception
+    {
+        driver.quit();
+        super.tearDown();
+    }
+
+
+    protected void rotateScreenPortrait()
+    {
+        driver.rotate((ScreenOrientation.PORTRAIT));
+    }
+
+    protected void rotateScreenLandscape()
+    {
+        driver.rotate((ScreenOrientation.LANDSCAPE));
+    }
+
+    protected void backgroundApp(int seconds)
+    {
+        driver.runAppInBackground(Duration.ofSeconds(seconds));
+    }
+
+private void skipWelcomePageForIOSApp()
+{
+    if (Platform.getInstance().isIOS()){
+        WelcomePageObject WelcomePageObject = new WelcomePageObject(driver);
+        WelcomePageObject.clickSkip();
+    }
+}
+
+}
---
+++ Lesson6Ex11andEx12/src/tests/MyListsTests.java
@@ -0,0 +1,58 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.Platform;
+import lib.ui.ArticlePageObject;
+import lib.ui.MyListsPageObject;
+import lib.ui.NavigationUI;
+import lib.ui.SearchPageObject;
+import lib.ui.factories.ArticlePageObjectFactory;
+import lib.ui.factories.MyListsPageObjectFactory;
+import lib.ui.factories.NavigationUIFactory;
+import lib.ui.factories.SearchPageObjectFactory;
+import org.junit.Test;
+
+public class MyListsTests extends CoreTestCase {
+
+    private static final String name_of_folder = "Learning programming";
+    @Test
+    public void testSaveFirstAricleToMyList()
+    {
+
+        SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
+
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Appium");
+        SearchPageObject.clickByArticleWithSubstring("Appium");
+        ArticlePageObject ArticlePageObject = ArticlePageObjectFactory.get(driver);
+        ArticlePageObject.waitForTitleElement();
+        String article_title = ArticlePageObject.getArticleTitle();
+
+        if(Platform.getInstance().isAndroid()){
+            ArticlePageObject.addArticleToMyList(name_of_folder);
+        } else {
+            ArticlePageObject.addArticlesToMySaved();
+        }
+
+        ArticlePageObject.closeArticle();
+        if (Platform.getInstance().isIOS()){
+            SearchPageObject.clickCancelSearch();
+        }
+
+        NavigationUI NavigationUI = NavigationUIFactory.get(driver);
+        NavigationUI.clickMyLists();
+        MyListsPageObject MyListsPageObject = MyListsPageObjectFactory.get(driver);
+
+
+        if (Platform.getInstance().isAndroid()){
+            MyListsPageObject.openFolderByName(name_of_folder);
+        }
+
+        MyListsPageObject.swipeByArticleToDelete(article_title);
+
+    }
+
+
+
+
+}
